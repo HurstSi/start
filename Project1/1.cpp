2d635e014e61d5158485e9f9824f138b9031cbbc
政平 司 <sizhengping@hotmail.com>
2018-11-02 23:00:22 +0800

    1

---
+++ Project1/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int x, y;
+	int m, n, r, g;
+	cout << "输入2个数x,y：" << endl;//输入提示信息
+	//start
+   //end
+	cout << "最大公约数：" << m << endl;
+	// 求最小公倍数
+//start
+ //end
+	cout << "最小公倍数：" << g << endl;
+	return 0;
+}++++++++++++++++
