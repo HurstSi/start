1b6cb13e8edcc3f739c9227c5f3e0d286fd6a663
政平 司 <sizhengping@hotmail.com>
2018-11-03 01:17:23 +0800

    7

---
+++ Project1/中值.cpp
@@ -0,0 +1,20 @@
+/*#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	int a, b, c, middle;
+	cin >> a >> b >> c;
+	if (a >= b && a <= c)
+		middle = a;
+	else if (b >= a && b <= c)
+		middle = b;
+	else if (a > b)
+		middle = b;
+	else if (a < b)
+		middle = a;
+	cout << middle << endl;
+	system("pause");
+	return 0;
+}
+*/
---
+++ Project1/求累加的和.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+	int n, j = 0, s = 0;
+	cout << "ÊäÈën:";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= i; j++)
+		{
+			s = s + j;
+		}
+	cout << "s=1+(1+2)+(1+2+3)+¡­+(1+2+3+¡­+" << n << ")=" << s << endl;
+	system("pause");
+	return 0;
+}*/
